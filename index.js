8af9f82e93752aedb21359ca746229bb82456b0a
ANyanCatFan <saitam.spatz@gmail.com>
2017-04-14 15:58:44 +0200

    Telebot instead of node-telegram-bot-api; Fixed an important bug; Begginings of wiktionary spell-checking

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.API_KEY, {
-  polling: true,
-  filepath: false
-});
+const TeleBot = require('telebot');
+const bot = new TeleBot(process.env.API_KEY);
 
 var messages = {};
 
-bot.onText(/^(?!\/fuck)/, (msg, exec) => {
-  messages[msg.chat.id] = {};
-  messages[msg.chat.id][msg.from.id] = msg.text.split(" ");
+bot.on("text", msg => {
+  if (msg.text != "/fuck" && msg.text != "/fuck@TheFuckTelegramBot") {
+    if (!messages[msg.chat.id]) messages[msg.chat.id] = {};
+    messages[msg.chat.id][msg.from.id] = msg.text.split(" ");
+    console.log(messages);
+  }
 });
 
-bot.onText(/\/fuck/, (msg, exec) => {
-  bot.sendMessage(msg.chat.id, messages[msg.chat.id][msg.from.id].join(" "));
+bot.on("/fuck", msg => {
+  console.log("fuck");
+  return bot.sendMessage(msg.chat.id, messages[msg.chat.id][msg.from.id].join(" "));
 });
+bot.connect();
+
+/*
+var now = new Date().getTime();
+const words = require("fs").readFileSync("./wiktionary", {
+  encoding: "utf8"
+}).split("\n");
+console.log(words, "\n", new Date().getTime() - now);
+
+now = new Date().getTime();
+for (var i = 1; i < words.length; i++) {
+  words[i] += words[i - 1];
+}
+console.log("\n", new Date().getTime() - now);
+*/
